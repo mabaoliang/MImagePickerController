85a369f2d9da50b5dab17c4369ff3f3755d0182f
mac <mac@macdeimac.local>
2018-09-20 17:52:49 +0800

    Initial Commit

---
+++ MImagePickerController/MImagePickerController.h
@@ -0,0 +1,19 @@
+//
+//  MImagePickerController.h
+//  MImagePickerController
+//
+//  Created by mac on 2018/9/20.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MImagePickerController.
+FOUNDATION_EXPORT double MImagePickerControllerVersionNumber;
+
+//! Project version string for MImagePickerController.
+FOUNDATION_EXPORT const unsigned char MImagePickerControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MImagePickerController/PublicHeader.h>
+
+
